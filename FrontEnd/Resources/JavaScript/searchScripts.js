e7907938cd21e9fa3263ccaecc04afdcf0d5ddfc
nhenault <nahe4217@colorado.edu>
2020-03-01 15:59:24 -0700

    Created framework for search page
    1. Created title and search bar form, still need to complete
       JavaScript function to pull results from database.
    
    2. Created table for results, still need to complete formating and
       finish the filters form within the table.

---
+++ FrontEnd/Resources/JavaScript/searchScripts.js
@@ -0,0 +1,12 @@
+/*
+    @Return void
+    @Param tableID - The ID of the reselts table being populated (what needs to be made visible after populating)
+    @Param tableBodyID - The ID of the body of the table being populated (where results are added)
+    The methods adds results from a database to a results table on the search page.
+*/
+function populateResults(tableID, tableBodyID)
+{
+    // Code to create and add results to the results table (tableBodyID)
+
+    // Code to make table visible after adding results to it (tableID)
+}
